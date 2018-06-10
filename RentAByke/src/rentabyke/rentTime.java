4ab1a555ff96a07f6957157f597e5f6a158a4930
NahuelEz2 <nahuelezequielcorrea@gmail.com>
2018-06-10 12:05:06 -0300

    Creé la clase RentTime
    Clase parecida a la clase Date, personalizada para este caso, donde la clase Date tiraba errores en el cálculo de semana (quise alojar la cantidad de semana en el parámetro de Mes que recibe el Objeto Date rentTime, pero tiraba error, recibía por parámetro 16 pero al aplicar método .grtMonth() devolvía 6) Considero que la tienda de bicis, al no tener promo por alquiler por mes, limita su alquiler hasta 3 semanas, por eso intenté alojar semanas en el parámetro Mes (de 0 a 11)

---
+++ RentAByke/src/rentabyke/rentTime.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rentabyke;
+
+/**
+ *
+ * @author Nahuel E Correa
+ * 
+ */
+public class RentTime {
+        
+        private int seconds = 0;
+        private int minutes = 0;
+        private int hours = 0;
+        private int days = 0;
+        private int weeks = 0;
+
+    public RentTime(int weeks, int days, int hours, int minutes, int seconds) {
+        
+        this.weeks = weeks;
+        this.days = days;
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+    public void setDays(int days) {
+        this.days = days;
+    }
+
+    public int getWeeks() {
+        return weeks;
+    }
+
+    public void setWeeks(int weeks) {
+        this.weeks = weeks;
+    }
+    }
