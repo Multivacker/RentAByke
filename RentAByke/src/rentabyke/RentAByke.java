34814865397383245e57596cfd5f3b8ffcfafbb5
NahuelEz2 <nahuelezequielcorrea@gmail.com>
2018-06-10 12:09:53 -0300

    Realicé simples tests

--- RentAByke/src/rentabyke/RentAByke.java
+++ RentAByke/src/rentabyke/RentAByke.java
@@ -20,25 +20,64 @@ public class RentAByke {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("La renta empezó el: " + rentStarts);
+        System.out.println("La renta empezó el: " + rentStarts + " OK");
+        System.out.println("");
+        System.out.println("La renta terminó el: " + rentEnds  + " OK");
+        System.out.println("");
+        System.out.println("Semanas: " + calculatesRentTime(rentStarts, rentEnds).getWeeks());
+        System.out.println("Días: " + calculatesRentTime(rentStarts, rentEnds).getDays());
+        System.out.println("Horas: " + calculatesRentTime(rentStarts, rentEnds).getHours());
+        System.out.println("Minutos: " + calculatesRentTime(rentStarts, rentEnds).getMinutes());
+        System.out.println("Segundos: " + calculatesRentTime(rentStarts, rentEnds).getSeconds());
+        System.out.println("");
+        System.out.println("CalculaHora_ El costo de la renta es de: $ " + calculateCostPerHour(calculatesRentTime(rentStarts, rentEnds)) + " OK");
+        System.out.println("");
+        System.out.println("CalculaDia_ El costo de la renta es de: $ " + calculateCostPerDay(calculatesRentTime(rentStarts, rentEnds)) + " OK");
+        System.out.println("");
+        System.out.println("CalculaSemana_ El costo del alquiler es es de: $ " + calculateCostPerWeek(calculatesRentTime(rentStarts, rentEnds)) + " OK");
+        System.out.println("");
+        System.out.println("AplicaDescuentoPlanFamiliar_El costo final del alquiler es de: $ " + applyDiscountForFamilyRental(rentCost, cantBikes));
+           
+    }
+    
+    /*Método que calcula la diferencia entre dos Objetos de la clase Date*/
+    public static RentTime calculatesRentTime(Date rentStarts, Date rentEnds){
         
-        System.out.println("La renta terminó el: " +rentEnds);
+        //Date rentTime = new Date();
         
-        System.out.println("El tiempo de la renta es: " + rentTime2);
+        int seconds=(int)((rentEnds.getTime()-rentStarts.getTime())/1000);
         
-        System.out.println("El costo de la renta es de: $ " + calculateCostPerHour(rentTime));
+        int weeks = 0;
+        int days = 0;
+        int hours = 0;
+        int minutes = 0;
         
-        System.out.println("El costo de la renta es de: $ " + calculateCostPerDay(rentTime));
+        if(seconds>604800) {
+            weeks = (int)Math.floor(seconds/604800);
+            seconds=seconds-(days*604800);
+        }
+        if(seconds>86400) {
+            days = (int)Math.floor(seconds/86400);
+            seconds=seconds-(days*86400);
+        }
+        if(seconds>3600) {
+            hours = (int)Math.floor(seconds/3600);
+            seconds = seconds-(hours*3600);
+        }
+        if(seconds>60) {
+            minutes = (int)Math.floor(seconds/60);
+            seconds = seconds-(minutes*60);
+        }
         
-        System.out.println("El costo de la renta es de: $ " + calculateCostPerWeek(rentTime));
+        RentTime rentTime = new RentTime(weeks, days, hours, minutes, seconds);
         
-        //System.out.println(calculatesDiscountForFamilyRental(rentCost));
+        return rentTime;
     }
     
     
     /*Método que calcula el costo de la renta por hora, tiene una tolerancia
     de 15 minutos, pasado ese tiempo se le cobrará el recargo por minutos excedidos*/
-    public static float calculateCostPerHour(Date rentTime){
+    public static float calculateCostPerHour(RentTime rentTime){
         
         float rentCostPerHour = (rentTime.getHours())*5;
         
@@ -57,13 +96,13 @@ public class RentAByke {
     
     
     /*Método que calcula el costo de la renta por Día*/
-    public static float calculateCostPerDay(Date rentTime){
+    public static float calculateCostPerDay(RentTime rentTime){
         
         float rentCostPerDay = 0;
                 
-        if(rentTime.getDay()<7){
+        if(rentTime.getDays()<7){
             
-            rentCostPerDay = (rentTime.getDay())*20;
+            rentCostPerDay = (rentTime.getDays())*20;
             
         }
        
@@ -72,55 +111,70 @@ public class RentAByke {
     
     
     /*Método que calcula el costo de la renta por Semana*/
-    public static float calculateCostPerWeek(Date rentTime){
+    public static float calculateCostPerWeek(RentTime rentTime){
+       
+        float rentCostPerWeek = rentTime.getWeeks()*60;
         
-       float rentCostPerWeek = (float)(rentTime.getDay()/7)*60;
+        float surchargePerDay;
+        
+        if(rentTime.getDays()%7!=0){
             
-       return rentCostPerWeek;
+            surchargePerDay = (rentTime.getDays()%7)*20;
+            
+            rentCostPerWeek = rentCostPerWeek + surchargePerDay;
+            
+        }
+        
+        return rentCostPerWeek;
     }
     
     
     /*Método que aplica el descuento por plan familiar*/
-    public float applyDiscountForFamilyRental(float rentCost){
+    public static double applyDiscountForFamilyRental(double rentCost, int cantBikes){
         
-        float discount = (float)((rentCost)*0.3);
+        if((cantBikes>=3)&&(cantBikes<=5)){
         
-        rentCost = rentCost-discount;
+        double discount = rentCost*0.3;
         
+        rentCost = rentCost - discount;
+        
+        }
         return rentCost;
     }
-    
-    /*Método que calcula la diferencia entre dos Objetos de la clase Date*/
-    public static Date calculatesRentTime(Date rentStarts, Date rentEnds){
-        
-        int timeDiference=(int)((rentEnds.getTime()-rentStarts.getTime())/1000);
- 
-        int days = 0;
-        int hours = 0;
-        int minutes = 0;
-        if(timeDiference>86400) {
-            days = (int)Math.floor(timeDiference/86400);
-            timeDiference=timeDiference-(days*86400);
-        }
-        if(timeDiference>3600) {
-            hours = (int)Math.floor(timeDiference/3600);
-            timeDiference = timeDiference-(hours*3600);
-        }
-        if(timeDiference>60) {
-            minutes = (int)Math.floor(timeDiference/60);
-            timeDiference = timeDiference-(minutes*60);
-        }
-        
-        System.out.println("Tiempo de renta: " + days + " dias, " + hours + " horas, " + minutes + " minutos y " + timeDiference + " segundos.");
-        
-        return rentTime = new Date(118, 5, days, hours, minutes, timeDiference);
-    }
-    
-    
-    private static Date rentStarts = new Date(118, 5,7, 9, 30, 0);
-    private static Date rentTime = new Date(0, 0, 15, 2, 30, 0);//de prueba
-    private static java.util.Date rentEnds = new java.util.Date();
-    private static Date rentTime2 = new Date();
 
+    public Date getRentStarts() {
+        return rentStarts;
+    }
+
+    public void setRentStarts(Date rentStarts) {
+        this.rentStarts = rentStarts;
+    }
+
+    public Date getRentEnds() {
+        return rentEnds;
+    }
+
+    public void setRentEnds(Date rentEnds) {
+        this.rentEnds = rentEnds;
+    }
+    //Test week
+    //private static Date rentStarts = new Date(118, 4, 24, 9, 30, 0);
+    //private static Date rentEnds = new Date(118, 5, 9, 15, 50, 14);
     
+    //Test day
+    //private static Date rentStarts = new Date(118, 5, 2, 9, 30, 0);
+    //private static Date rentEnds = new Date(118, 5, 9, 15, 50, 14);
+    
+    //Test hour
+    //private static Date rentStarts = new Date(118, 5, 9, 9, 30, 0);
+    //private static Date rentEnds = new Date(118, 5, 9, 15, 50, 14);
+    
+    //Test real
+    //Asigna el momento en el que se hace click tanto en botón para empezar el alquiler, como en el botón de devolver 
+    private static Date rentStarts = new Date();
+    private static Date rentEnds = new Date();
+    private static int cantBikes = 4;
+    private static double rentCost = calculateCostPerWeek(calculatesRentTime(rentStarts, rentEnds));
+    
+   
 }
