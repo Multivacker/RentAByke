624e5b267c7a9e34dc03b9fdea5d70a3ff2d8c44
NahuelEz2 <nahuelezequielcorrea@gmail.com>
2018-06-11 07:43:48 -0300

    Desarrollé la clase Bike

--- RentAByke/src/rentabyke/Bike.java
+++ RentAByke/src/rentabyke/Bike.java
@@ -11,5 +11,62 @@ package rentabyke;
  * 
  */
 public class Bike {
+
+    public Bike(long ID, String brand, String model, int wheelSize, boolean fondable) {
+        
+        this.brand = brand;
+        this.model = model;
+        this.wheelSize = wheelSize;
+        this.fondable = fondable;
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getWheelSize() {
+        return wheelSize;
+    }
+
+    public void setWheelSize(int wheelSize) {
+        this.wheelSize = wheelSize;
+    }
+
+    public boolean isFondable() {
+        return fondable;
+    }
+
+    public void setFondable(boolean fondable) {
+        this.fondable = fondable;
+    }
+
+    @Override
+    public String toString() {
+        return "Bike{" + "ID = " + ID + ", brand = " + brand + ", model = " + 
+                model + ", wheelSize = " + wheelSize + ", fondable = " + fondable + '}';
+    }
+    
+private final long ID = 0; 
+private String brand = "";
+private String model = "";
+private int wheelSize = 0;
+private boolean fondable = false;
     
 }
+
